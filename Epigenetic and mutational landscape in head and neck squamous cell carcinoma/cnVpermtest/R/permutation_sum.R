5209342cffa7c5de09be3a22e7a40c1952c6eba4
Yi Chen <ychen@bcg.biostat.wisc.edu>
2019-01-18 10:04:41 -0600

    Add comments

--- Epigenetic and mutational landscape in head and neck squamous cell carcinoma/cnVpermtest/R/permutation_sum.R
+++ Epigenetic and mutational landscape in head and neck squamous cell carcinoma/cnVpermtest/R/permutation_sum.R
@@ -7,34 +7,35 @@
 #' @keywords cnVpermtest
 #' @export
 #' @examples
+#' @import GenomicRanges
 #' permutation_sum()
 
-permutation_sum<-function(data1,data2,cnadata){
+permutation_sum <- function(data1,data2,cnadata){
   
-  col<-c("REGION_ID","CHROM","START","STOP",
+  col <- c("REGION_ID","CHROM","START","STOP",
          "NUM_LOCI","CONSTITUENT_SIZE","Intensity","Intensity2",
          "EnhancerRank1","Overlap_Genes","Proximal_Genes",
          "Closest_Gene","EnhancerRank2","IsSuper")
-  colnames(data1)<-col
-  colnames(data2)<-col
-  data1<-subset(data1,!(data1$CHROM %in% c("chrX","chrY","chrx","chry")))
-  data2<-subset(data2,!(data2$CHROM %in% c("chrX","chrY","chrx","chry")))
+  colnames(data1) <- col
+  colnames(data2) <- col
+  data1 <- subset(data1,!(data1$CHROM %in% c("chrX","chrY","chrx","chry")))
+  data2 <- subset(data2,!(data2$CHROM %in% c("chrX","chrY","chrx","chry")))
   
-  chrname.1s<-as.character(data1$CHROM)
-  start.1s<-as.numeric(data1$START)
-  end.1s<-as.numeric(data1$STOP)
-  gr.1s<-GRanges(seqnames=chrname.1s,ranges=IRanges(start=start.1s,end=end.1s))
+  chrname.1s <- as.character(data1$CHROM)
+  start.1s <- as.numeric(data1$START)
+  end.1s <- as.numeric(data1$STOP)
+  gr.1s <- GRanges(seqnames=chrname.1s,ranges=IRanges(start=start.1s,end=end.1s))
   
-  chrname.2s<-as.character(data2$CHROM)
-  start.2s<-as.numeric(data2$START)
-  end.2s<-as.numeric(data2$STOP)
-  gr.2s<-GRanges(seqnames=chrname.2s,ranges=IRanges(start=start.2s,end=end.2s))
+  chrname.2s <- as.character(data2$CHROM)
+  start.2s <- as.numeric(data2$START)
+  end.2s <- as.numeric(data2$STOP)
+  gr.2s <- GRanges(seqnames=chrname.2s,ranges=IRanges(start=start.2s,end=end.2s))
   
-  hits<-findOverlaps(gr.1s, gr.2s)
+  hits <- findOverlaps(gr.1s, gr.2s)
   overlap12 <- intersect(gr.1s, gr.2s)
-  overlap<-lapply(cnadata, function(x) lapply(x, function(x) intersect(overlap12, x)))
-  num.base<-lapply(overlap, function(x) lapply(x, function(x) sum(width(x))))
-  n1<-sapply(num.base, function(x) sum(unlist(x)))
+  overlap <- lapply(cnadata, function(x) lapply(x, function(x) intersect(overlap12, x)))
+  num.base <- lapply(overlap, function(x) lapply(x, function(x) sum(width(x))))
+  n1 <- sapply(num.base, function(x) sum(unlist(x)))
   n1
   return(list("n1"=n1, "overlap12"=overlap12))
   
--- Epigenetic and mutational landscape in head and neck squamous cell carcinoma/cnVpermtest/R/tcgacnv_ampdel.R
+++ Epigenetic and mutational landscape in head and neck squamous cell carcinoma/cnVpermtest/R/tcgacnv_ampdel.R
@@ -11,66 +11,67 @@
 #' @examples
 #' @import doParallel
 #' @import ggplot2
+#' @import GenomicRanges
 #' tcgacnv_ampdel()
 
 tcgacnv_ampdel<-function(cnvdatalist,segmentmean_name,segment_cutoff,permtime,core){
   ## Classify if studying amplification or deletion;
   if (segment_cutoff>0){
-    CNV<-lapply(cnvdatalist,function(x) x[,segmentmean_name]>=segment_cutoff)
+    CNV <- lapply(cnvdatalist,function(x) x[,segmentmean_name]>=segment_cutoff)
     
   }
   else {
-    CNV<-lapply(cnvdatalist,function(x) x[,segmentmean_name]<=segment_cutoff)
+    CNV <- lapply(cnvdatalist,function(x) x[,segmentmean_name]<=segment_cutoff)
   }
   
   
-  listdata<-mapply(FUN=function(x,CNV){cbind(x,CNV)},cnvdatalist, CNV,SIMPLIFY = FALSE)
+  listdata <- mapply(FUN=function(x,CNV){cbind(x,CNV)},cnvdatalist, CNV,SIMPLIFY = FALSE)
   ## Only consider CNV=1
-  tcga.cnv<-lapply(listdata, function(x) subset(x,x$CNV==TRUE))
-  tcga.cnv<-unname(tcga.cnv[sapply(tcga.cnv, function(x) nrow(x)>0)])
-  tcga.cnv<-lapply(tcga.cnv, function(x) subset(x,!(x$Chromosome %in% c("chrX","chrY","chrx","chry"))))
+  tcga.cnv <- lapply(listdata, function(x) subset(x,x$CNV==TRUE))
+  tcga.cnv <- unname(tcga.cnv[sapply(tcga.cnv, function(x) nrow(x)>0)])
+  tcga.cnv <- lapply(tcga.cnv, function(x) subset(x,!(x$Chromosome %in% c("chrX","chrY","chrx","chry"))))
   
   ## Find available chromosome names for each sample
-  chr.name.p<-lapply(tcga.cnv, function(x) as.character(unique(x$Chromosome)))
+  chr.name.p <- lapply(tcga.cnv, function(x) as.character(unique(x$Chromosome)))
   
   ## Calculate the range of permutation: range.p
-  range.total<-mapply(FUN=function(x,chr){x[x$Chromosome %in% chr,]},listdata, chr.name.p,SIMPLIFY = FALSE)
-  range.p<-lapply(range.total, function(x) by(x,x$Chromosome,function(x) c(min(x$Start),max(x$End))))
+  range.total <- mapply(FUN=function(x,chr){x[x$Chromosome %in% chr,]},listdata, chr.name.p,SIMPLIFY = FALSE)
+  range.p <- lapply(range.total, function(x) by(x,x$Chromosome,function(x) c(min(x$Start),max(x$End))))
   ## Delete the NULL chromosome and size
-  range.p<-mapply(FUN = function(x,name){x[name]},range.p, chr.name.p,
+  range.p <- mapply(FUN = function(x,name){x[name]},range.p, chr.name.p,
                   SIMPLIFY = FALSE)
   ## Calculate the number of observations for each chromosome and delete NA chromosome
-  size.p<-lapply(tcga.cnv, function(x) by(x,x$Chromosome,function(x) length(x$Start)))
-  size.p<-mapply(FUN = function(x,name){x[name]},size.p, chr.name.p,
+  size.p <- lapply(tcga.cnv, function(x) by(x,x$Chromosome,function(x) length(x$Start)))
+  size.p <- mapply(FUN = function(x,name){x[name]},size.p, chr.name.p,
                  SIMPLIFY = FALSE)
   
-  len.p<-sapply(size.p, length)
+  len.p <- sapply(size.p, length)
   set.seed(100)
-  cl<-makeCluster(core)
+  cl <- makeCluster(core)
   registerDoParallel(cl)
   
-  start.p<-foreach(i=1:length(tcga.cnv)) %:%
+  start.p <- foreach(i=1:length(tcga.cnv)) %:%
     foreach(j=1:len.p[i]) %:% 
-    foreach(k=1:unlist(size.p[[i]])[j]) %dopar%{
-      cnv.p<-tcga.cnv[[i]][tcga.cnv[[i]]$Chromosome==chr.name.p[[i]][j],][k,]
-      range<-unlist(range.p[[i]][j])
-      start<-sample(range[1]:(range[2]-cnv.p$End+cnv.p$Start),permtime,replace=TRUE)
-      end<-start+cnv.p$End-cnv.p$Start
-      Chrname<-rep(as.character(chr.name.p[[i]][j]),permtime)
+    foreach(k=1:unlist(size.p[[i]])[j]) %dopar% {
+      cnv.p <- tcga.cnv[[i]][tcga.cnv[[i]]$Chromosome==chr.name.p[[i]][j],][k,]
+      range <- unlist(range.p[[i]][j])
+      start <- sample(range[1]:(range[2]-cnv.p$End+cnv.p$Start),permtime,replace=TRUE)
+      end <- start+cnv.p$End-cnv.p$Start
+      Chrname <- rep(as.character(chr.name.p[[i]][j]),permtime)
       data.frame(start,end,Chrname)
       
       
     }
   
   
-  df<-foreach(i=1:permtime)%:%
+  df <- foreach(i=1:permtime)%:%
     foreach(l=1:length(tcga.cnv)) %:%
     foreach(j=1:len.p[l],.combine='rbind') %:%
     foreach(k=1:unlist(size.p[l])[j],.combine='rbind')%dopar% {
       data.frame(start.p[[l]][[j]][[k]])[i,]
     }
   
-  df<-lapply(df,function(x) lapply(x, function(x) GRanges(as.character(x$Chrname),ranges=IRanges(x$start,x$end))))
+  df <- lapply(df,function(x) lapply(x, function(x) GRanges(as.character(x$Chrname),ranges=IRanges(x$start,x$end))))
   
   on.exit(stopCluster(cl))
   
