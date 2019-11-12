5400c7601eb7a97a2da09a9a6ef8fb0e07b05cf2
Yi Chen <ychen@bcg.biostat.wisc.edu>
2019-11-12 16:45:41 -0600

    update

--- Epigenetic and mutational landscape in head and neck squamous cell carcinoma/cnVpermtest/R/permhist.R
+++ Epigenetic and mutational landscape in head and neck squamous cell carcinoma/cnVpermtest/R/permhist.R
@@ -1,19 +1,22 @@
 #' A function to make histogram for CNA permutation overlap with Super Enhancers
 #'
-#' This function allows you to make histogram for CNA permutation.
+#' @description This function allows you to make histogram for CNA permutation.
 #' @param permoverlap A dataframe with first column as vector bases of permutated CNA overlaps with Super Enhancers.
-#' @param obsoverlap The observed bases of CNA segment overlap with Super Enhancers 
+#' @param obsoverlap The observed bases of CNA segment overlap with Super Enhancers
 #' @param title Title of the histogram
-#' @param position Position of the lable of observed overlap value, right or left of the marked dashed line
+#' @param position Position of the lable of observed overlap value, right or left of the marked dashed line, default is Right
+#' \itemize{
+#'         \item{\code{"right"}}{ the label of observed value will be positioned to the right}
+#'         \item{\code{"left"}} { the label of observed value will be positioned to the right}
+#'     }
 #' @param num Number of bases to be away from the marked dashed line
-#' @keywords cnVpermtest
 #' @export
 #' @examples
 #' @import ggplot2
-#' permhist()
+
 
 permhist<-function(permoverlap,obsoverlap,title,position="right",num){
-  
+
   if (position =="right"){
     ggplot(data.frame(permoverlap[,1]),aes(permoverlap[,1]))+geom_histogram()+geom_vline(xintercept=obsoverlap,colour="red",linetype="dashed")+
       geom_text(y=20,x=obsoverlap-num,cex=6,label=paste("observed value is",obsoverlap),colour="red")+
@@ -22,15 +25,15 @@ permhist<-function(permoverlap,obsoverlap,title,position="right",num){
       theme(axis.text=element_text(size=16),axis.title=element_text(size=20,face="bold"))
   }
   else if (position=="left"){
-    
+
     ggplot(data.frame(permoverlap[,1]),aes(permoverlap[,1]))+geom_histogram()+geom_vline(xintercept=obsoverlap,colour="red",linetype="dashed")+
       geom_text(y=20,x=obsoverlap-num,cex=6,label=paste("observed value is",obsoverlap),colour="red")+
       labs(x="Permutation Overlap Bases")+ggtitle(title)+
       theme(plot.title = element_text(hjust = 0.5,size=22,face="bold"))+
       theme(axis.text=element_text(size=16),axis.title=element_text(size=20,face="bold"))
-    
+
   }
-  
+
 }
 
 
